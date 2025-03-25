f66a59dd87c637c04b9be91972464dea18220af6
Mio Ebisu <ebisumio@gmail.com>
2025-03-25 22:24:35 +0000

    I have done so many cool things nailed it.

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
-//Ink Splash game
-
-//Create canvas.
+//キャンバスを作成する
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
+const startButton = document.getElementById("start-button");
 
-/*Fill canvas background color white to get white pixel data.
- Default canvas background is transparent so it won't return anything.  */
+const startGame = () => {
+  setTimeout(endGame, 10000);
+  canvas.addEventListener("click", onCanvasClick);
+  countDownTimer();
+};
+
+startButton.addEventListener("click", startGame);
+
+//透明の背景を白くぬる
 ctx.fillStyle = "white";
 ctx.fillRect(0, 0, canvas.width, canvas.height);
 
 //Check the canvas size.
 console.log(`canvas size ${canvas.width} : ${canvas.height}`);
 
-//ロード後３秒後に「マウスをクリックしてペイントしよう！10秒でどのくらいペイントできるかな？！」が表示される
-
-//Create ink images and array.
+//インクのイメージの変数を作成
 const blue = new Image();
 blue.src = "images/blue.png";
 const green = new Image();
@@ -26,6 +30,7 @@ pink.src = "images/pink.png";
 const yellow = new Image();
 yellow.src = "images/yellow.png";
 
+//インクの配列を作成
 const inkImages = [blue, green, skyblue, pink, yellow];
 
 //インクをinkImages配列から取り出し、サイズをランダムに変更
@@ -37,21 +42,24 @@ const getInkImages = () => {
   return { img, size };
 };
 
-// const inkData = getInkImages();
-// const img = inkData.img;
-// const size = inkData.size;
-
 const drawInkImage = (x, y, img, size) => {
   ctx.drawImage(img, x - size / 2, y - size / 2, size, size);
 };
 
-canvas.addEventListener("click", (e) => {
+const onCanvasClick = (e) => {
+  e.preventDefault();
   const { img, size } = getInkImages();
   drawInkImage(e.offsetX, e.offsetY, img, size);
   const colorPixels = getPixeldata();
   getPixeldata();
   showPercentage(colorPixels);
-});
+  // Move this into a button on click method
+  // you might need som estate flag, like `isPlaying`
+};
+
+const removeHandler = () => {
+  canvas.removeEventListener("click", onCanvasClick);
+};
 
 const checkColor = (rgba) => {
   if (rgba.r === 255 && rgba.g === 255 && rgba.b === 255) {
@@ -90,90 +98,22 @@ const showPercentage = (colorPixels) => {
   finishText.innerHTML = `You painted ${percentageRounded}%!`;
 };
 
-//   const percentageRounded = Math.floor(percentage * 100) / 100;
-//   console.log(`${percentageRounded}%`);
-
-//   const finishText = document.getElementById("finishText");
-//   finishText.innerHTML = `You painted ${percentageRounded}%!`;
-// });
-
-// canvas.addEventListener("click", (e) => {
-
-//   const img = inkImages[Math.floor(Math.random() * inkImages.length)];
-
-//   const size = Math.random() * 100 + 200;
-//   ctx.drawImage(img, e.offsetX - size / 2, e.offsetY - size / 2, size, size);
-
-//   const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-//   const data = imageData.data;
-//   let colorCount = 0;
-//   //create object with key rgba and value of color number
-//   for (let i = 0; i < data.length; i += 4) {
-//     const rgba = {};
-//     rgba.r = data[i];
-//     rgba.g = data[i + 1];
-//     rgba.b = data[i + 2];
-//     rgba.a = data[i + 3];
-//     //return true(white) when color number is 255
-//     const isWhite = checkColor(rgba);
-//     //count non white color
-//     if (!isWhite) {
-//       colorCount += 1;
-//     }
-//   }
-
-//   console.log(colorCount);
-
-//   //display number in xx.xx format
-//   const percentage = (colorCount / (canvas.width * canvas.height)) * 100;
-//   const percentageRounded = Math.floor(percentage * 100) / 100;
-//   console.log(`${percentageRounded}%`);
-
-//   const finishText = document.getElementById("finishText");
-//   finishText.innerHTML = `You painted ${percentageRounded}%!`;
-// });
-
-// //check rgb color number
-// const checkColor = (rgba) => {
-//   if (rgba.r === 255 && rgba.g === 255 && rgba.b === 255) {
-//     return true;
-//   } else {
-//     return false;
-//   }
-// };
-
 //animation starts when game ends
 const getEndGameText = document.getElementById("endgameText-animation");
 
-canvas.addEventListener("click", () => {
-  const showText = () => {
-    getEndGameText.classList.add("show");
-  };
-  setTimeout(showText, 10000);
-  removeEventListener("click");
-});
+const endGame = () => {
+  removeHandler();
+  getEndGameText.classList.add("show");
+};
 
-// const showGameMessage = () => {
-//   getEndGameText.classList.add("show");
-// };
-
-// setTimeout(getEndGameText, 10000);
-
-// setTimeout(() => {
-//   getEndGameText.classList.add("show");
-// }, 10000);
-
-// const clickHandler = () => {
-//   setTimeout(showGameMessage(), 10000);
-//   removeEventListener;
-// };
-
-// addEventListener("click", () => {
-//   clickHandler();
-// });
-
-//終了時の音
-// const playSound = () => {
-//   const alertSound = new Audio("audio/whistle.mp3");
-//   alertSound.play();
-// };
+const timer = document.getElementById("timer");
+const countDownTimer = () => {
+  let i = 9;
+  const intervalID = setInterval(() => {
+    timer.innerHTML = i;
+    i--;
+    if (i === -1) {
+      clearInterval(intervalID);
+    }
+  }, 1000);
+};
